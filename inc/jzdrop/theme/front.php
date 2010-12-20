673bfccefcaf13e4a6908e82c511e3ddfe543f0d
Jonathan Zacsh <jzacsh@gmail.com>
2010-12-20 02:05:34 -0500

    decent enough to post live.

--- inc/jzdrop/theme/front.php
+++ inc/jzdrop/theme/front.php
@@ -2,10 +2,15 @@
 /**
  * Custom page.
  */
-$opts = array('title' => '@jzacsh', 'class' => 'twitter');
+$opts = array('title' => '@jzacsh', 'class' => 'twitter', 'rel' => 'nofollow', 'target' => '_blank');
 $links[] = l('twitter', 'http://www.twitter.com/jzacsh/', $opts);
 
-$opts = array('class' => 'github');
+/* DIASPORA:
+$opts = array('class' => 'diaspora', 'rel' => 'nofollow', 'target' => '_blank');
+$links[] = l('diaspora', 'https://joindiaspora.com/people/4d06cfa62c174313c9000eb4', $opts);
+*/
+
+$opts = array('class' => 'github', 'rel' => 'nofollow', 'target' => '_blank');
 $links[] = l('github', 'http://www.github.com/jzacsh/', $opts);
 
 foreach ($links as $data) {
@@ -19,12 +24,13 @@ jzdrop_add_css(JZDROP . '/theme/css/front.css');
 ?>
 <div id="main">
 
-  <div class="notes">
+  <div id="content">
+    <h3 class="whoami">Jonathan Zacsh</h3>
+    <p class="story">I'm a computer science student, currently working as a Drupal web developer.</p>
+  </div><!--//#content-->
+
+  <div id="notes">
     <?php print $places_list; ?>
   </div><!--//.notes-->
-
-  <div class="content">
-    <p class="story">I'm a computer science student, currently working as a Drupal web developer.</p>
-  </div><!--//.content-->
 
 </div><!--//#main-->
