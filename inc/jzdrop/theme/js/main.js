45fa09a415f7db7b4055bd4a7d53465677c02929
Jonathan Zacsh <jzacsh@gmail.com>
2011-01-08 22:45:28 -0500

    added some documentation for next time :)

--- inc/jzdrop/theme/js/main.js
+++ inc/jzdrop/theme/js/main.js
@@ -6,9 +6,10 @@
 var JzDrop = JzDrop || { behaviors : {}, settings : {} };
 
 // run each function stored in settings.
+//@TODO: this seems to be fail whale
 $(document).ready(function(context) {
     JzDrop.behaviors.each(behave, function(context) {
       $(this).attach();
-      console.log("behave is: "+behave);
+      console.log("behave is: "+behave); //@TODO: remove me!!
     });
 });
